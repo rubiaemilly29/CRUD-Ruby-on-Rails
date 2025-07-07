0b4b924617e831b153dc0666af50b3378e653274
Rubia Braga <rubiaemilly29@gmail.com>
2025-07-07 20:13:17 -0300

    Add requisito 5

--- tech-interview-backend-entry-level-main/app/sidekiq/mark_cart_as_abandoned_job.rb
+++ tech-interview-backend-entry-level-main/app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -1,7 +1,14 @@
 class MarkCartAsAbandonedJob
   include Sidekiq::Job
 
-  def perform(*args)
-    # TODO Impletemente um Job para gerenciar, marcar como abandonado. E remover carrinhos sem interação. 
-  end
+  def perform
+    # Marca como abandonados os carrinhos sem interação há mais de 3 horas
+    Cart.where("updated_at < ? AND abandoned = ?", 3.hours.ago, false).find_each do |cart|
+      cart.mark_as_abandoned!
+    end
+
+    # Remove carrinhos abandonados há mais de 7 dias
+    Cart.where("abandoned = ? AND updated_at < ?", true, 7.days.ago).find_each do |cart|
+      cart.remove_if_abandoned!
+    end
 end
